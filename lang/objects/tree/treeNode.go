549cdbfe4922fbf7d1c4fcf04a6079ad62de0b57
zhuzhenjie <zhuzj94@gmail.com>
2022-08-29 00:26:30 +0800

    fix: struct

--- lang/objects/tree/treeNode.go
+++ lang/objects/tree/treeNode.go
@@ -10,20 +10,104 @@ type treeNode struct {
 	right *treeNode
 }
 
+/**
+自定义的工厂函数
+
+注意：返回的是局部变量的地址
+*/
 func createNode(value int) *treeNode {
 	return &treeNode{value: value}
 }
 
+/**
+初步面向对象
+
+个人理解， who do(param) return
+
+给结构体定义了方法？？？
+
+func (接收者) 方法名(参数) 返回值
+
+*/
+func (node *treeNode) print() {
+	if node == nil {
+		fmt.Println("node is nil")
+		return
+	}
+	fmt.Println("value is", node.value)
+}
+
+func (node *treeNode) setValue(value int) {
+	if node == nil {
+		fmt.Println("node is nil")
+		return
+	}
+	node.value = value
+}
+
+func (node *treeNode) getValue() int {
+	return node.value
+}
+
+/**
+遍历
+*/
+func (node *treeNode) dfs(action func(node *treeNode)) {
+	if node == nil {
+		return
+	}
+
+	node.left.dfs(action)
+	action(node)
+	node.right.dfs(action)
+
+}
+
 func main() {
-	root := treeNode{}
+	/*
+			2
+		  /  \
+		1      3
+	*/
+
+	root := treeNode{value: 3, left: nil, right: nil}
 
 	fmt.Println(root)
 
-	node := createNode(3)
-	fmt.Println(node)
+	// node := createNode(3)
+	// fmt.Println(node)
+	//
+	// root.left = node
+	// fmt.Println(root)
+	root.left = &treeNode{1, nil, nil}
+	root.right = createNode(3)
 
-	root.left = node
+	fmt.Println("hello world")
 
-	fmt.Println(root)
+	fmt.Println("--- print")
+
+	root.print()
+
+	root.setValue(200)
+
+	root.print()
+
+	root.left.setValue(100)
+
+	root.right.setValue(300)
+
+	root.left.print()
+	root.right.print()
+
+	root.left.left.print()
+
+	fmt.Println("dfs ... ")
+
+	root.dfs(func(node *treeNode) {
+		if node == nil {
+			return
+		}
+		fmt.Println("node's value is:", node.getValue())
+	})
 
 }
